c07abbc8019a31b4b0099adeb307818d81eee174
Ilina <ibg@berkeley.edu>
2018-04-28 17:05:57 -0700

    messaging

---
+++ app/src/main/java/com/example/jonathansun5/eatmeet/Comment.java
@@ -0,0 +1,59 @@
+package com.example.jonathansun5.eatmeet;
+
+import java.util.Date;
+
+// custom class made for storing a message. you can update this class
+public class Comment {
+
+    public String text;
+    public String username;
+    public long date;
+
+    Comment(String text, String username, Long date) {
+        this.text = text;
+        this.username = username;
+        this.date = date;
+    }
+
+    Comment() {
+        this.text = "hello world";
+        this.username = "dummy";
+        this.date = new Date().getTime();
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Long getDate() {
+        return date;
+    }
+
+    // returns a string indicating how long ago this post was made
+    protected String elapsedTimeString() {
+        long diff = new Date().getTime() - date;
+        long seconds = diff / 1000;
+        long minutes = seconds / 60;
+        long hours = minutes / 60;
+        long days = hours / 24;
+        int daysInt = Math.round(days);
+        int hoursInt = Math.round(hours);
+        int minutesInt = Math.round(minutes);
+        if (daysInt == 1) {
+            return "1 day";
+        } else if (daysInt > 1) {
+            return Integer.toString(daysInt) + " days";
+        } else if (hoursInt == 1) {
+            return "1 hour";
+        } else if (hoursInt > 1) {
+            return Integer.toString(hoursInt) + " hours";
+        } else {
+            return "less than an hour";
+        }
+    }
+}
+
